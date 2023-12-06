f4499fc14c5489ecbb4cb7d507f77910606a132b
mlin9 <mlin9@protonmail.com>
2023-12-06 04:09:21 -0500

    code golfing

--- Day1.java
+++ Day1.java
@@ -10,39 +10,81 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 
 public class Day1 {
+
+    private static final String REGEX_NUMS = "(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)|(zero)|\\d";
+    
     public static void main(String[] args) throws URISyntaxException, IOException {
-        sumPartOne();
-        sumPartTwo();
+        printSums();
     }
 
-    public static void sumPartOne() throws IOException {
-        File file = new File("Day1.txt");
-        Path path1 = Paths.get(file.getAbsolutePath());
-        List<String> lines1 = Files.readAllLines(path1, StandardCharsets.UTF_8);
-        String[] document1 = (String[]) lines1.toArray(new String[lines1.size()]);
-        System.out.println(sumDigits(document1));
+    public static void printSums() throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get((new File("Day1.txt")).getAbsolutePath()),
+                StandardCharsets.UTF_8);
+        System.out.println(sumPartOne(lines.toArray(new String[lines.size()])));
+        System.out.println(sumPartTwo(lines.toArray(new String[lines.size()])));
     }
 
-    public static void sumPartTwo() throws IOException {
-        File file = new File("Day1.txt");
-        Path path2 = Paths.get(file.getAbsolutePath());
-        List<String> lines2 = Files.readAllLines(path2, StandardCharsets.UTF_8);
-        String[] document2 = (String[]) lines2.toArray(new String[lines2.size()]);
-        System.out.println(sumMatches(document2));
-    }
-
-    public static int sumMatches(String[] document) {
+    public static int sumPartOne(String[] document) {
         int sum = 0;
-        String regex = "(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)|(zero)|\\d";
-        for (int i = 0; i < document.length; i++) {
+        for (String line : document) {
             ArrayList<Integer> digits = new ArrayList<>();
-            Matcher matcher = Pattern.compile(regex).matcher(document[i]);
-            while(matcher.find()) {
-                digits.add(parseString(matcher.group()));
+            for (Character character : line.toCharArray()) {
+                int digit = parseChar(character);
+
+                if (digit > 0) {
+                    digits.add(digit);
+                }
             }
-            sum = sum + digits.getFirst() * 10 + digits.getLast();
+            sum += digits.getFirst() * 10 + digits.getLast();
         }
         return sum;
+    }
+
+    public static int sumPartTwo(String[] document) {
+        int sum = 0;
+        for (String line : document) {
+            ArrayList<String> matches = regexFirstLast(line, REGEX_NUMS);
+            int first = parseString(matches.getFirst()) * 10;
+            int last = parseString(matches.getLast());
+            sum += first + last;
+        }
+        return sum;
+    }
+
+    public static ArrayList<String> regexFirstLast(String input, String pattern) {
+        ArrayList<String> matches = new ArrayList<>();
+        Matcher matcher = Pattern.compile(pattern).matcher(input);
+        while (matcher.find()) {
+            matches.add(matcher.group());
+        }
+        return new ArrayList<String>(Arrays.asList(matches.getFirst(), matches.getLast()));
+    }
+
+    public static int parseChar(char input) {
+        switch ((int) input) {
+            case 48:
+                return 0;
+            case 49:
+                return 1;
+            case 50:
+                return 2;
+            case 51:
+                return 3;
+            case 52:
+                return 4;
+            case 53:
+                return 5;
+            case 54:
+                return 6;
+            case 55:
+                return 7;
+            case 56:
+                return 8;
+            case 57:
+                return 9;
+            default:
+                return -1;
+        }
     }
 
     public static int parseString(String input) {
@@ -68,50 +110,7 @@ public class Day1 {
             case "zero":
                 return 0;
             default:
-                return parseInt(input.charAt(0));
-        }
-    }
-
-    public static int sumDigits(String[] document) {
-        int sum = 0;
-        for (int i = 0; i < document.length; i++) {
-            ArrayList<Integer> digits = new ArrayList<>();
-            for (int j = 0; j < document[i].length(); j++) {
-                int digit = parseInt(document[i].charAt(j));
-
-                if (digit > 0) {
-                    digits.add(digit);
-                }
-            }
-            sum = sum + digits.getFirst() * 10 + digits.getLast();
-        }
-        return sum;
-    }
-
-    public static int parseInt(char input) {
-        switch ((int) input) {
-            case 48:
-                return 0;
-            case 49:
-                return 1;
-            case 50:
-                return 2;
-            case 51:
-                return 3;
-            case 52:
-                return 4;
-            case 53:
-                return 5;
-            case 54:
-                return 6;
-            case 55:
-                return 7;
-            case 56:
-                return 8;
-            case 57:
-                return 9;
-            default:
-                return -1;
+                return parseChar(input.charAt(0));
         }
     }
 }
