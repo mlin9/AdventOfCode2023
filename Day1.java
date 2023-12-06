51456a6582c81777cb01c3f880cf504b0e9c02a2
mlin9 <mlin9@protonmail.com>
2023-12-06 04:23:09 -0500

    code golfing

--- Day1.java
+++ Day1.java
@@ -1,18 +1,21 @@
 package AdventOfCode2023;
 
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.*;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day1 {
 
     private static final String REGEX_NUMS = "(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)|(zero)|\\d";
-    
+
     public static void main(String[] args) throws URISyntaxException, IOException {
         printSums();
     }
@@ -30,10 +33,8 @@ public class Day1 {
             ArrayList<Integer> digits = new ArrayList<>();
             for (Character character : line.toCharArray()) {
                 int digit = parseChar(character);
-
-                if (digit > 0) {
+                if (digit > 0)
                     digits.add(digit);
-                }
             }
             sum += digits.getFirst() * 10 + digits.getLast();
         }
@@ -54,37 +55,9 @@ public class Day1 {
     public static ArrayList<String> regexFirstLast(String input, String pattern) {
         ArrayList<String> matches = new ArrayList<>();
         Matcher matcher = Pattern.compile(pattern).matcher(input);
-        while (matcher.find()) {
+        while (matcher.find())
             matches.add(matcher.group());
-        }
         return new ArrayList<String>(Arrays.asList(matches.getFirst(), matches.getLast()));
-    }
-
-    public static int parseChar(char input) {
-        switch ((int) input) {
-            case 48:
-                return 0;
-            case 49:
-                return 1;
-            case 50:
-                return 2;
-            case 51:
-                return 3;
-            case 52:
-                return 4;
-            case 53:
-                return 5;
-            case 54:
-                return 6;
-            case 55:
-                return 7;
-            case 56:
-                return 8;
-            case 57:
-                return 9;
-            default:
-                return -1;
-        }
     }
 
     public static int parseString(String input) {
@@ -111,6 +84,33 @@ public class Day1 {
                 return 0;
             default:
                 return parseChar(input.charAt(0));
+        }
+    }
+
+    public static int parseChar(char input) {
+        switch ((int) input) {
+            case 48:
+                return 0;
+            case 49:
+                return 1;
+            case 50:
+                return 2;
+            case 51:
+                return 3;
+            case 52:
+                return 4;
+            case 53:
+                return 5;
+            case 54:
+                return 6;
+            case 55:
+                return 7;
+            case 56:
+                return 8;
+            case 57:
+                return 9;
+            default:
+                return -1;
         }
     }
 }
--- Day2.java
+++ Day2.java
@@ -4,10 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -23,14 +21,10 @@ public class Day2 {
     }
 
     static void printPartOne() throws IOException {
-        File file = new File("Day2.txt");
-        Path path = Paths.get(file.getAbsolutePath());
-        List<String> list = Files.readAllLines(path, StandardCharsets.UTF_8);
+        List<String> list = Files.readAllLines(Paths.get((new File("Day2.txt")).getAbsolutePath()),
+                StandardCharsets.UTF_8);
         String[] strings = (String[]) list.toArray(new String[list.size()]);
-        Map<String, Integer> bag = new HashMap<>();
-        bag.put("red", 12);
-        bag.put("green", 13);
-        bag.put("blue", 14);
+        Map<String, Integer> bag = Map.of("red", 12, "green", 13, "blue", 14);
         System.out.println(sumPartOne(strings, bag));
     }
 
@@ -41,10 +35,9 @@ public class Day2 {
             for (String set : document[i].trim().toLowerCase().split(";")) {
                 for (String subset : set.replaceAll("\\s", "").split(",")) {
                     String[] cubeCounts = regexAll(subset, REGEX_CUBES);
-                    for (int j = 0; j < cubeCounts.length && hasCapacity; j++) {
+                    for (int j = 0; j < cubeCounts.length && hasCapacity; j++)
                         hasCapacity = bag.get(regex(cubeCounts[j], REGEX_COLOR)) >= Integer
                                 .parseInt(regex(cubeCounts[j], REGEX_INT));
-                    }
                 }
             }
             sum = sum + (hasCapacity ? i + 1 : 0);
@@ -55,17 +48,15 @@ public class Day2 {
     public static String[] regexAll(String input, String pattern) {
         ArrayList<String> matches = new ArrayList<>();
         Matcher matcher = Pattern.compile(pattern).matcher(input);
-        while (matcher.find()) {
+        while (matcher.find())
             matches.add(matcher.group());
-        }
         return matches.toArray(new String[matches.size()]);
     }
 
     public static String regex(String input, String pattern) {
         Matcher matcher = Pattern.compile(pattern).matcher(input);
-        if (matcher.find()) {
+        if (matcher.find())
             return matcher.group();
-        }
         return null;
     }
 }
