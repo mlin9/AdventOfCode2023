4c655ab7c7a9af20313849ee50d0d276cd34b628
mlin9 <mlin9@protonmail.com>
2023-12-16 08:31:49 -0500

    completed day 2 and golfed

--- Day1.java
+++ Day1.java
@@ -14,103 +14,103 @@ import java.util.regex.Pattern;
 
 public class Day1 {
 
-    private static final String REGEX_NUMS = "(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)|(zero)|\\d";
+  private static final String REGEX_NUMS = "(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)|(zero)|\\d";
 
-    public static void main(String[] args) throws URISyntaxException, IOException {
-        printSums();
-    }
+  public static void main(String[] args) throws URISyntaxException, IOException {
+    printSums();
+  }
 
-    public static void printSums() throws IOException {
-        List<String> lines = Files.readAllLines(Paths.get((new File("Day1.txt")).getAbsolutePath()),
-                StandardCharsets.UTF_8);
-        System.out.println(sumPartOne(lines.toArray(new String[lines.size()])));
-        System.out.println(sumPartTwo(lines.toArray(new String[lines.size()])));
-    }
+  public static void printSums() throws IOException {
+    List<String> lines = Files.readAllLines(Paths.get((new File("Day1.txt")).getAbsolutePath()),
+        StandardCharsets.UTF_8);
+    System.out.println(sumPartOne(lines.toArray(new String[lines.size()])));
+    System.out.println(sumPartTwo(lines.toArray(new String[lines.size()])));
+  }
 
-    public static int sumPartOne(String[] document) {
-        int sum = 0;
-        for (String line : document) {
-            ArrayList<Integer> digits = new ArrayList<>();
-            for (Character character : line.toCharArray()) {
-                int digit = parseChar(character);
-                if (digit > 0)
-                    digits.add(digit);
-            }
-            sum += digits.getFirst() * 10 + digits.getLast();
-        }
-        return sum;
+  public static int sumPartOne(String[] document) {
+    int sum = 0;
+    for (String line : document) {
+      ArrayList<Integer> digits = new ArrayList<>();
+      for (Character character : line.toCharArray()) {
+        int digit = parseChar(character);
+        if (digit > 0)
+          digits.add(digit);
+      }
+      sum += digits.get(0) * 10 + digits.get(digits.size() - 1);
     }
+    return sum;
+  }
 
-    public static int sumPartTwo(String[] document) {
-        int sum = 0;
-        for (String line : document) {
-            ArrayList<String> matches = regexFirstLast(line, REGEX_NUMS);
-            int first = parseString(matches.getFirst()) * 10;
-            int last = parseString(matches.getLast());
-            sum += first + last;
-        }
-        return sum;
+  public static int sumPartTwo(String[] document) {
+    int sum = 0;
+    for (String line : document) {
+      ArrayList<String> matches = regexFirstLast(line, REGEX_NUMS);
+      int first = parseString(matches.get(0)) * 10;
+      int last = parseString(matches.get(matches.size() - 1));
+      sum += first + last;
     }
+    return sum;
+  }
 
-    public static ArrayList<String> regexFirstLast(String input, String pattern) {
-        ArrayList<String> matches = new ArrayList<>();
-        Matcher matcher = Pattern.compile(pattern).matcher(input);
-        while (matcher.find())
-            matches.add(matcher.group());
-        return new ArrayList<String>(Arrays.asList(matches.getFirst(), matches.getLast()));
-    }
+  public static ArrayList<String> regexFirstLast(String input, String pattern) {
+    ArrayList<String> matches = new ArrayList<>();
+    Matcher matcher = Pattern.compile(pattern).matcher(input);
+    while (matcher.find())
+      matches.add(matcher.group());
+    return new ArrayList<String>(Arrays.asList(matches.get(0), matches.get(matches.size() - 1)));
+  }
 
-    public static int parseString(String input) {
-        switch (input) {
-            case "one":
-                return 1;
-            case "two":
-                return 2;
-            case "three":
-                return 3;
-            case "four":
-                return 4;
-            case "five":
-                return 5;
-            case "six":
-                return 6;
-            case "seven":
-                return 7;
-            case "eight":
-                return 8;
-            case "nine":
-                return 9;
-            case "zero":
-                return 0;
-            default:
-                return parseChar(input.charAt(0));
-        }
+  public static int parseString(String input) {
+    switch (input) {
+      case "one":
+        return 1;
+      case "two":
+        return 2;
+      case "three":
+        return 3;
+      case "four":
+        return 4;
+      case "five":
+        return 5;
+      case "six":
+        return 6;
+      case "seven":
+        return 7;
+      case "eight":
+        return 8;
+      case "nine":
+        return 9;
+      case "zero":
+        return 0;
+      default:
+        return parseChar(input.charAt(0));
     }
+  }
 
-    public static int parseChar(char input) {
-        switch ((int) input) {
-            case 48:
-                return 0;
-            case 49:
-                return 1;
-            case 50:
-                return 2;
-            case 51:
-                return 3;
-            case 52:
-                return 4;
-            case 53:
-                return 5;
-            case 54:
-                return 6;
-            case 55:
-                return 7;
-            case 56:
-                return 8;
-            case 57:
-                return 9;
-            default:
-                return -1;
-        }
+  public static int parseChar(char input) {
+    switch ((int) input) {
+      case 48:
+        return 0;
+      case 49:
+        return 1;
+      case 50:
+        return 2;
+      case 51:
+        return 3;
+      case 52:
+        return 4;
+      case 53:
+        return 5;
+      case 54:
+        return 6;
+      case 55:
+        return 7;
+      case 56:
+        return 8;
+      case 57:
+        return 9;
+      default:
+        return -1;
     }
+  }
 }
--- Day2.java
+++ Day2.java
@@ -6,57 +6,75 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Day2 {
-    private final static String REGEX_CUBES = "\\d+((red)|(green)|(blue))";
-    private final static String REGEX_COLOR = "(red)|(green)|(blue)";
-    private final static String REGEX_INT = "\\d+";
+  private final static String REGEX_CUBES = "\\d+((red)|(green)|(blue))";
+  private final static String REGEX_COLOR = "(red)|(green)|(blue)";
+  private final static String REGEX_INT = "\\d+";
 
-    public static void main(String[] args) throws IOException {
-        printSums();
-    }
+  public static void main(String[] args) throws IOException {
+    printSums();
+  }
 
-    static void printSums() throws IOException {
-        List<String> list = Files.readAllLines(Paths.get((new File("Day2.txt")).getAbsolutePath()),
-                StandardCharsets.UTF_8);
-        String[] strings = (String[]) list.toArray(new String[list.size()]);
-        Map<String, Integer> bag = Map.of("red", 12, "green", 13, "blue", 14);
-        System.out.println(sumPartOne(strings, bag));
-    }
+  static void printSums() throws IOException {
+    List<String> list = Files.readAllLines(Paths.get((new File("Day2.txt")).getAbsolutePath()),
+        StandardCharsets.UTF_8);
+    String[] strings = (String[]) list.toArray(new String[list.size()]);
+    Map<String, Integer> bag = Map.of("red", 12, "green", 13, "blue", 14);
+    System.out.println(sumPartOne(strings, bag));
+    System.out.println(sumPartTwo(strings));
+  }
 
-    static int sumPartOne(String[] document, Map<String, Integer> bag) {
-        int sum = 0;
-        for (int i = 0; i < document.length; i++) {
-            boolean hasCapacity = true;
-            for (String set : document[i].trim().toLowerCase().split(";")) {
-                for (String subset : set.replaceAll("\\s", "").split(",")) {
-                    String[] cubeCounts = regexAll(subset, REGEX_CUBES);
-                    for (int j = 0; j < cubeCounts.length && hasCapacity; j++)
-                        hasCapacity = bag.get(regex(cubeCounts[j], REGEX_COLOR)) >= Integer
-                                .parseInt(regex(cubeCounts[j], REGEX_INT));
-                }
-            }
-            sum = sum + (hasCapacity ? i + 1 : 0);
+  static int sumPartOne(String[] document, Map<String, Integer> bag) {
+    int sum = 0;
+    for (int i = 0; i < document.length; i++) {
+      boolean hasCapacity = true;
+      for (String set : document[i].trim().toLowerCase().split(";"))
+        for (String subset : set.replaceAll("\\s", "").split(",")) {
+          String[] cubes = regexAll(subset, REGEX_CUBES);
+          for (int j = 0; j < cubes.length && hasCapacity; j++)
+            hasCapacity = bag.get(regex(cubes[j], REGEX_COLOR)) >= Integer
+                .parseInt(regex(cubes[j], REGEX_INT));
         }
-        return sum;
+      sum += hasCapacity ? i + 1 : 0;
     }
+    return sum;
+  }
 
-    public static String[] regexAll(String input, String pattern) {
-        ArrayList<String> matches = new ArrayList<>();
-        Matcher matcher = Pattern.compile(pattern).matcher(input);
-        while (matcher.find())
-            matches.add(matcher.group());
-        return matches.toArray(new String[matches.size()]);
+  static int sumPartTwo(String[] document) {
+    int sum = 0;
+    for (String line : document) {
+      Map<String, Integer> maximums = new HashMap<String, Integer>(Map.of("red", 0, "green", 0, "blue", 0));
+      int power = 1;
+      for (String set : line.trim().toLowerCase().split(";"))
+        for (String subset : set.replaceAll("\\s", "").split(","))
+          for (String cube : regexAll(subset, REGEX_CUBES))
+            if (maximums.get(regex(cube, REGEX_COLOR)) < Integer.parseInt(regex(cube, REGEX_INT)))
+              maximums.put(regex(cube, REGEX_COLOR), Integer.parseInt(regex(cube, REGEX_INT)));
+      for (int value : maximums.values())
+        power *= value;
+      sum += power;
     }
+    return sum;
+  }
 
-    public static String regex(String input, String pattern) {
-        Matcher matcher = Pattern.compile(pattern).matcher(input);
-        if (matcher.find())
-            return matcher.group();
-        return null;
-    }
+  public static String[] regexAll(String input, String pattern) {
+    ArrayList<String> matches = new ArrayList<>();
+    Matcher matcher = Pattern.compile(pattern).matcher(input);
+    while (matcher.find())
+      matches.add(matcher.group());
+    return matches.toArray(new String[matches.size()]);
+  }
+
+  public static String regex(String input, String pattern) {
+    Matcher matcher = Pattern.compile(pattern).matcher(input);
+    if (matcher.find())
+      return matcher.group();
+    return null;
+  }
 }
